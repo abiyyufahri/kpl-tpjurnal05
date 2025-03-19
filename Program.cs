f2f3070c6f923b4fd57d76339afab2791abb23ce
abiyyufahri <abiyyurahid20@gmail.com>
2025-03-19 07:04:49 +0700

    [200~menambahkan class PemrosesData dengan method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class PemrosesData
+{
+    public static void DapatkanNilaiTerbesar<T>(T x, T y, T z)
+    {
+        dynamic X = (dynamic)y;
+        dynamic Y = (dynamic)x;
+        dynamic Z = (dynamic)z;
+
+
+        if (X > Y && X > Z)
+        {
+            Console.WriteLine("Nilai terbesar adalah " + X);
+        }
+        else if (Y > X && Y > Z)
+        {
+            Console.WriteLine("Nilai terbesar adalah " + Y);
+        }
+        else
+        {
+            Console.WriteLine("Nilai terbesar adalah " + Z);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        PemrosesData.DapatkanNilaiTerbesar<dynamic>(12, 34, 56);
+    }
+}
